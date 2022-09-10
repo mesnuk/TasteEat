e9f7cd4c8becd3318229e027aff2154239c98787
unknown <samsuser570@gmail.com>
2022-09-10 20:09:44 +0300

    add request

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React, {DOMElement} from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from './components/header/header'
 import {SectionWelcome} from './components/welcome-section/section-welcome'
-import {SectionAboutUs} from './components/about-us-section/info'
+import {DataService} from "./services/data-service";
+import {CardProps} from "./components/about-us-section/types";
+import {SectionAboutUs} from "./components/about-us-section/info";
 
 
 
 
 export const App = () : JSX.Element =>{
+let [cardProps, setCardProps]: [CardProps[], any] = useState([]);
+
+    const getCards = (): void => {
+        new DataService().getCards()
+            .then((res) => setCardProps(res))    }
+
+    useEffect(() => {
+        getCards();
+    }, [])
 
   return (
     <div className="App">
       <Header />
       <SectionWelcome />
-      <SectionAboutUs />
+      <SectionAboutUs infoCards={cardProps} textCards={[]}/>
     </div>
   );
 }
--- src/components/about-us-section/info.tsx
+++ src/components/about-us-section/info.tsx
@@ -1,40 +1,12 @@
-import React, {useEffect, useState} from 'react'
-import {CardProps, TextBlockProps, JSONCardsProps} from "./types";
+import React from 'react'
+import {CardProps, JSONCardsProps, TextBlockProps} from "./types";
 import {ShortInfoCard} from './short-info-card'
-import {Description, TextBlock} from './description'
-
-import shortTextCards from '../../data/text-cards.json'
-import shortInfoCards from '../../data/short-info-cards.json'
-
-import axios from "axios";
-
-
-
-
-const textCards: TextBlockProps[] = JSON.parse(JSON.stringify(shortTextCards));
-const infoCards: CardProps[] = JSON.parse(JSON.stringify(shortInfoCards))
-
-class DataService {
-    getData() {
-        return axios.get('../../data/short-info-cards.json', {
-            headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        } ).then(response => console.log(response))
-    }
-}
-
+import { Description } from './description'
 
 
 
 const Cards: React.FC<JSONCardsProps> = ( {arr}  ) => {
-    const k = new DataService().getData();
-
-
-
-
-
+console.log(arr);
     return (
         <div className='short-icon-side'>
             {arr.map( el => <ShortInfoCard key={el.key} heading={el.heading!}
@@ -45,8 +17,11 @@ const Cards: React.FC<JSONCardsProps> = ( {arr}  ) => {
 
 
 
-export const SectionAboutUs : React.FC<{}> = () => {
+export const SectionAboutUs : React.FC<{
+    infoCards: CardProps[];
+    textCards: TextBlockProps[];
 
+}> = ({infoCards, textCards}) => {
 
     return (
         <section className='section-about-us'>
