c05c8332109a00d424a8505b8e7710bc5f27056e
unknown <samsuser570@gmail.com>
2022-09-10 20:52:55 +0300

    refresh request

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,35 @@ import React, {useEffect, useState} from 'react';
 import Header from './components/header/header'
 import {SectionWelcome} from './components/welcome-section/section-welcome'
 import {DataService} from "./services/data-service";
-import {CardProps} from "./components/about-us-section/types";
+import {CardProps, TextBlockProps} from "./components/about-us-section/types";
 import {SectionAboutUs} from "./components/about-us-section/info";
 
 
 
 
 export const App = () : JSX.Element =>{
-let [cardProps, setCardProps]: [CardProps[], any] = useState([]);
+const [cardProps, setCardProps]: [CardProps[], any] = useState([]);
+const [textProps, setTextProps]: [TextBlockProps[], any] = useState([]);
 
-    const getCards = (): void => {
-        new DataService().getCards()
+    const getInfoCards = (): void => {
+        new DataService().getInfoCards()
             .then((res) => setCardProps(res))    }
 
+    const getTextCards = (): void => {
+        new DataService().getTextCards()
+            .then((res) => setTextProps(res))    }
+
     useEffect(() => {
-        getCards();
+        getInfoCards();
+        getTextCards();
+
     }, [])
 
   return (
     <div className="App">
       <Header />
       <SectionWelcome />
-      <SectionAboutUs infoCards={cardProps} textCards={[]}/>
+      <SectionAboutUs infoCards={cardProps} textCards={textProps} />
     </div>
   );
 }
